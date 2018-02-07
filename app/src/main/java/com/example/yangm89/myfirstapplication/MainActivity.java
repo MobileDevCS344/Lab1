c26ae78b183f749b17793bd642f7d6926aaaaa23
blindbycolors <mnyang1991@gmail.com>
2018-02-06 22:14:46 -0600

    updated portrait layout

--- app/src/main/java/com/example/yangm89/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/yangm89/myfirstapplication/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.yangm89.myfirstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(getResources().getConfiguration().orientation==
+                Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_landscape);
+        } else{
+            setContentView(R.layout.activity_portrait);
+        }
+        //this leaves the keyboard hidden on load
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
