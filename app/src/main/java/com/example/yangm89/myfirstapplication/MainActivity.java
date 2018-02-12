5ceca7a5872c7978b6b5e709ee74ea19184ca405
blindbycolors <mnyang1991@gmail.com>
2018-02-12 15:59:25 -0600

    lab3 updates

--- app/src/main/java/com/example/yangm89/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/yangm89/myfirstapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,33 @@ public class MainActivity extends AppCompatActivity {
         //this leaves the keyboard hidden on load
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+
+        //initialize random card during start up
+        String[] sArray = {"c", "d", "h", "s"};
+        String card1, card2, card3;
+        Random random = new Random();
+        int rIndex = random.nextInt(4 - 0) + 0;
+        int rCard = random.nextInt(27 - 1) + 1;
+        card1 = new String(sArray[rIndex] + rCard);
+        ImageView c1Image = (ImageView) (findViewById(R.id.imageView_card1));
+        rIndex = random.nextInt(4 - 0) + 0;
+        rCard = random.nextInt(27 - 1) + 1;
+        card2 = new String(sArray[rIndex] + rCard);
+        while(card2.equals(card1)){
+            rIndex = random.nextInt(4 - 0) + 0;
+            rCard = random.nextInt(27 - 1) + 1;
+            card2 = sArray[rIndex] + rCard;
+        }
+        ImageView c2Image = (ImageView) (findViewById(R.id.imageView_card2));
+        rIndex = random.nextInt(4 - 0) + 0;
+        rCard = random.nextInt(27 - 1) + 1;
+        card3 = new String(sArray[rIndex] + rCard);
+        while(card3.equals(card2) || card3.equals(card1)){
+            rIndex = random.nextInt(4 - 0) + 0;
+            rCard = random.nextInt(27 - 1) + 1;
+            card3 = sArray[rIndex] + rCard;
+        }
+        ImageView c3Image = (ImageView) (findViewById(R.id.imageView_card2));
     }
 }
