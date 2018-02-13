5b77604f780f87f744045e5fe88f844834c4459e
blindbycolors <mnyang1991@gmail.com>
2018-02-13 13:49:51 -0600

    updates to layout for lab3

--- app/src/main/java/com/example/yangm89/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/yangm89/myfirstapplication/MainActivity.java
@@ -8,47 +8,49 @@ import android.widget.ImageView;
 
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    int[] cards = {R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5,
+                    R.drawable.c6, R.drawable.c7, R.drawable.c8, R.drawable.c9, R.drawable.cj,
+                    R.drawable.cq, R.drawable.ck, R.drawable.ct,
+                    R.drawable.d1, R.drawable.d2, R.drawable.d3, R.drawable.d4, R.drawable.d5,
+                    R.drawable.d6, R.drawable.d7, R.drawable.d8, R.drawable.d9, R.drawable.dj,
+                    R.drawable.dq, R.drawable.dk, R.drawable.dt,
+                    R.drawable.h1, R.drawable.h2, R.drawable.h3, R.drawable.h4, R.drawable.h5,
+                    R.drawable.h6, R.drawable.h7, R.drawable.h8, R.drawable.h9, R.drawable.hj,
+                    R.drawable.hq, R.drawable.hk, R.drawable.ht,
+                    R.drawable.s1, R.drawable.s2, R.drawable.s3, R.drawable.s4, R.drawable.s5,
+                    R.drawable.s6, R.drawable.s7, R.drawable.s8, R.drawable.s9, R.drawable.sj,
+                    R.drawable.sq, R.drawable.sk, R.drawable.st};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(getResources().getConfiguration().orientation==
+        if (getResources().getConfiguration().orientation ==
                 Configuration.ORIENTATION_LANDSCAPE) {
             setContentView(R.layout.activity_landscape);
-        } else{
+        } else {
             setContentView(R.layout.activity_portrait);
         }
         //this leaves the keyboard hidden on load
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
-
-        //initialize random card during start up
-        String[] sArray = {"c", "d", "h", "s"};
-        String card1, card2, card3;
-        Random random = new Random();
-        int rIndex = random.nextInt(4 - 0) + 0;
-        int rCard = random.nextInt(27 - 1) + 1;
-        card1 = new String(sArray[rIndex] + rCard);
-        ImageView c1Image = (ImageView) (findViewById(R.id.imageView_card1));
-        rIndex = random.nextInt(4 - 0) + 0;
-        rCard = random.nextInt(27 - 1) + 1;
-        card2 = new String(sArray[rIndex] + rCard);
-        while(card2.equals(card1)){
-            rIndex = random.nextInt(4 - 0) + 0;
-            rCard = random.nextInt(27 - 1) + 1;
-            card2 = sArray[rIndex] + rCard;
+        Random r = new Random();
+        int randomIndexOne = r.nextInt(53);
+        int randomIndexTwo = r.nextInt(53);
+        while(randomIndexTwo == randomIndexOne){
+            randomIndexTwo = r.nextInt(53);
         }
-        ImageView c2Image = (ImageView) (findViewById(R.id.imageView_card2));
-        rIndex = random.nextInt(4 - 0) + 0;
-        rCard = random.nextInt(27 - 1) + 1;
-        card3 = new String(sArray[rIndex] + rCard);
-        while(card3.equals(card2) || card3.equals(card1)){
-            rIndex = random.nextInt(4 - 0) + 0;
-            rCard = random.nextInt(27 - 1) + 1;
-            card3 = sArray[rIndex] + rCard;
+        int randomIndexThree = r.nextInt(53);
+        while(randomIndexOne == randomIndexThree || randomIndexTwo == randomIndexThree){
+            randomIndexThree = r.nextInt(53);
         }
-        ImageView c3Image = (ImageView) (findViewById(R.id.imageView_card2));
+        ImageView viewC1 = (ImageView) findViewById(R.id.imageView_card1);
+        ImageView viewC2 = (ImageView) findViewById(R.id.imageView_card2);
+        ImageView viewC3 = (ImageView) findViewById(R.id.imageView_card3);
+        viewC1.setImageResource(cards[randomIndexOne]);
+        viewC2.setImageResource(cards[randomIndexTwo]);
+        viewC3.setImageResource(cards[randomIndexThree]);
     }
 }
